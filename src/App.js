8d9254ffc0487e3d62b16e937435ced585992769
FredrikDalianis <78905580+FredrikDalianis@users.noreply.github.com>
2024-01-14 18:42:59 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import CardItem from './Card';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const searchJson = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setSearch(event.target.search.value);
+    await fetchData();
+  }
+
+  useEffect(() => {
+   fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(searchJson + search);
+      const json = await response.json();
+      console.log(json.meals);
+      setRecipes(json.meals);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form 
+      onSubmit={handleSubmit}
+      >
+        <label style={{fontWeight:"bold"}}>Search Recipe :   </label>
+        <input
+     style={{ textAlign:"center", borderRadius:"1em",textDecoration:"", color:"black" }}
+          
+          className='searchfield'
+          type='search'
+          name='search'
+        />
+        <button 
+     style={{ fontStyle:"Courier",fontWeight:"bold",textAlign:"center",margin:"1em", borderRadius:"3.33em",padding:"0.5em",textDecoration:"", color:"black" }}
+        
+        type='submit'>Go !</button>
+      </form>
+      {recipes?.map((recipe) => (
+      <CardItem 
+      title={recipe.strMeal} 
+      description={recipe.strInstructions}
+      image={recipe.strMealThumb} 
+      >
+    
+        </CardItem>
+      ))}
+{/*       <div className="recipeResult">
+        <h2>Search result for: {search}</h2>
+        <ul
+        style={{ listStyle:"none"}}>
+          {recipes?.map((recipe) => (
+            <li key={recipe.idMeal}>
+              <h3>{recipe.strMeal}</h3>
+              <img 
+              src={recipe.strMealThumb} 
+              alt={recipe.strMeal} 
+              style={{borderRadius:"5em",height:260, width:260}}/>
+              <p
+              style={{ textAlign:"left", padding:"4em",textDecoration:"", color:"black" }}
+              >{recipe.strInstructions}</p>
+            </li>
+          ))}
+        </ul>
+      </div> */}
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Card.js
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function CardItem(props) {
+
+  return (
+    <Card style={{
+      alignItems:"center",
+      width: '48rem' ,
+      backgroundColor: 'white',
+      padding: '4em',
+      }}>
+      <Card.Img 
+      variant="top" src={ props.image }
+      style={{borderRadius:"5em",height:260, width:260}}
+      />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text
+        style={{ textAlign:"left", padding:"4em",textDecoration:"", color:"black" }}
+        >
+        {props.description }
+        </Card.Text>
+       {/* <Button variant="primary"></Button> */}
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardItem;
